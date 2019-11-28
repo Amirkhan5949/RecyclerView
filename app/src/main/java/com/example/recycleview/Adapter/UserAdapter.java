60dc90c998185825122cb1439e9797867a0d98c1
Amirkhan5949 <ak4729176@gmail.com>
2019-11-28 23:46:11 +0530

    Onclick completed

--- app/src/main/java/com/example/recycleview/Adapter/UserAdapter.java
+++ app/src/main/java/com/example/recycleview/Adapter/UserAdapter.java
@@ -5,7 +5,9 @@ import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.recycleview.R;
 import com.example.recycleview.model.User;
@@ -36,10 +38,19 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
 
     @Override
     public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
-        User user=list.get(position);
+        final User user=list.get(position);
         holder.name.setText(user.getName());
         holder.status.setText(user.getStatus());
+        //Picasso.get().load(user.getImage()).into(holder.imageView);
         Picasso.get().load(user.getImage()).into(holder.imageView);
+        holder.layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "Name :"+user.getName()+"\n"+"Status :"+user.getStatus(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
 
     }
 
@@ -54,11 +65,13 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
     class UserViewHolder extends RecyclerView.ViewHolder {
         CircleImageView imageView;
         TextView name,status;
+        LinearLayout layout;
         public UserViewHolder(@NonNull View itemView) {
             super(itemView);
             imageView=itemView.findViewById(R.id.image);
             name=itemView.findViewById(R.id.name);
             status=itemView.findViewById(R.id.status);
+            layout=itemView.findViewById(R.id.layout);
 
         }
     }
